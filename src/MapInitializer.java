f975f9e88a15ba0401d17155fba1eab75e776c49
JavaThugNGG <ildarjava@mail.ru>
2025-01-06 16:47:24 +0300

    refactor: Some of the logic in class "Simulation" has been moved to "MapInitializer" class.

---
+++ src/MapInitializer.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.Map;
+
+public class MapInitializer {
+    public MapInitializer() {};
+
+    public void initializeMap(Map<Coordinates, Entity> map, List<Grass> grasses, List<Herbivore> herbivores) {
+        initializeEmptyCells(map);
+        spawnGrass(map, grasses,4,10);
+        spawnGrass(map, grasses,4,19);
+        spawnHerbivore(map, herbivores,7, 3);
+        spawnHerbivore(map, herbivores,5, 4);
+    }
+
+
+    private void initializeEmptyCells(Map<Coordinates, Entity> map) {
+        for (int row = 0; row < 10; row++) {
+            for (int column = 0; column < 20; column++) {
+                Coordinates coordinate = new Coordinates(row, column);  //создали координату
+                Entity emptyPlace = new EmptyPlace(coordinate, "⬛");      //создали пустую клетку
+                map.put(coordinate, emptyPlace);  //добавили пустую клетку по координате(ключу) в мапу
+            }
+        }
+    }
+
+    private void spawnGrass(Map<Coordinates, Entity> map, List<Grass> grasses, int row, int column) {
+        Coordinates spawnCoordinates = new Coordinates(row,column);
+        Grass grass = new Grass(spawnCoordinates, "\uD83C\uDF3F");
+        map.put(spawnCoordinates, grass);
+        grasses.add(grass);
+    }
+
+    private void spawnHerbivore(Map<Coordinates, Entity> map, List<Herbivore> herbivores, int row, int column) {
+        Coordinates spawnCoordinates = new Coordinates(row, column);
+        Herbivore herbivore = new Herbivore(spawnCoordinates, "\uD83D\uDC30");
+        map.put(spawnCoordinates, herbivore);
+        herbivores.add(herbivore);
+    }
+}
--- src/Simulation.java
+++ src/Simulation.java
@@ -2,42 +2,38 @@ import java.util.*;
 
 
 public class Simulation {
-    private  List<Coordinates> coordinatesList = new ArrayList<>();     //список всех координат
-    private  List<Entity> emptyPlacesList = new ArrayList<>();          //список пустых объектов, которые стоят на этих координатах
-
-    private  Map<Coordinates, Entity> map = new TreeMap<>();            //мапа для карты
-    private  int grassAlive = 1;
-    private  Entity herbivore;
+    private Map<Coordinates, Entity> map = new TreeMap<>();            //мапа для карты
+    private List<Herbivore> herbivores = new ArrayList<>();;
     private List<Grass> grasses = new ArrayList<>();
 
-    public Simulation() {
-    }
-
+    public Simulation() {};
 
     public void startSimulation() {
-        initializeEmptyMap();
+        MapInitializer mapInitializer = new MapInitializer();
+        mapInitializer.initializeMap(map, grasses, herbivores);
         do {;
             printMap();
             eatGrassIfAble();
-        } while (grassAlive != 0);
-        printMap();             //конечное состояние карты вывели
+        } while (true);
     }
 
+
     public void eatGrassIfAble() {
-        List<Coordinates> path = followPath();
-        path = followPath();
-        if (!path.isEmpty()) {
-            move(path);
+        for (Herbivore herbivore : herbivores) {
+            List<Coordinates> path = findPath(herbivore);
+            if (!path.isEmpty()) {
+                moveHerbivore(path, herbivore);
+            }
         }
     }
 
-    public List<Coordinates> followPath() {
+    public List<Coordinates> findPath(Herbivore herbivore) {
         PathFinder pathFinder = new PathFinder();
         return pathFinder.findPathToGrass(map, herbivore.getCoordinates());
     }
 
 
-    public void move(List<Coordinates> path) {
+    public void moveHerbivore(List<Coordinates> path, Herbivore herbivore) {
             Coordinates newCoordinates = path.getFirst();
             Coordinates oldCoordinates = herbivore.coordinates;
             herbivore.coordinates = newCoordinates;
@@ -45,43 +41,6 @@ public class Simulation {
             Entity emptyPlace = new EmptyPlace(oldCoordinates, "⬛");
             map.put(oldCoordinates, emptyPlace);
     }
-
-
-    public void initializeEmptyMap() {
-        initializeEmptyCells();
-        spawnGrass(8,2);
-        spawnGrass(4,7);
-        spawnHerbivore();
-    }
-
-
-    public  void initializeEmptyCells() {
-        for (int row = 0; row < 10; row++) {
-            for (int column = 0; column < 20; column++) {
-                Coordinates coordinate = new Coordinates(row, column);  //создали координату
-                Entity emptyPlace = new EmptyPlace(coordinate, "⬛");      //создали пустую клетку
-                map.put(coordinate, emptyPlace);  //добавили пустую клетку по координате(ключу) в мапу
-            }
-        }
-    }
-
-
-    public void spawnGrass(int row, int column) {
-        Coordinates spawnCoordinates = new Coordinates(row,column);
-        Grass grass = new Grass(spawnCoordinates, "\uD83C\uDF3F");
-        map.put(spawnCoordinates, grass);
-        grasses.add(grass);
-    }
-
-
-    public void spawnHerbivore() {
-        Coordinates spawnCoordinates = new Coordinates(7,3);
-        herbivore = new Herbivore(spawnCoordinates, "\uD83D\uDC30");
-        map.put(spawnCoordinates, herbivore);
-    }
-
-
-
 
 
     public void printMap() {

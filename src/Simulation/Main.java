5a6f91fb6121eacf2a28846119dbe6f6de533f31
JavaThugNGG <ildarjava@mail.ru>
2025-01-17 15:27:57 +0300

    refactor: Thread creating "simulationThread" was moved into own method

--- src/Simulation/Main.java
+++ src/Simulation/Main.java
@@ -26,36 +26,7 @@ public class Main {
             return;
         }
 
-        Thread simulationThread = new Thread(() -> {
-            while (!isEnd) {                                         //цикл организован здесь а не в классе Simulation, флаг isEnd подтягивается оттуда
-                synchronized (pauseLock) {
-                    try {
-                        while (isPaused) {
-                            pauseLock.wait();
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        return;
-                    }
-                }
-
-                if (!isEnd) {
-                    simulation.startSimulation();                  //одна отрисовка мапы (один ход)
-                }
-
-                try {
-                    Thread.sleep(1200); // Задержка для визуализации
-                } catch (InterruptedException e) {
-                    System.out.println("Поток simulationThread был прерван во время сна!");
-                    Thread.currentThread().interrupt();
-                    return;
-                }
-                isEnd = simulation.isEnd();              //обновили флаг (условие выхода из цикла)
-            }
-            simulation.printLast();                      //вывели мапу в самом конце когда волк сожрал зайца
-            isExit = true;
-            System.out.println("Симуляция закончена! Введите цифру от 1 до 3 ддя выхода");
-        });
+        Thread simulationThread = createSimulationThread(simulation);
 
         runMenu(simulationThread);
     }
@@ -105,17 +76,21 @@ public class Main {
                     case 2 -> {
                         if (isSimulationStarted) {
                             isPaused = true;
-                            System.out.println("Симуляция приостановлена.");
+                            synchronized (printLock) {
+                                System.out.println("Симуляция приостановлена.");
+                            }
                         } else {
                             System.out.println("\nОшибка! Сначала запустите симуляцию!\n");
                         }
                     }
                     case 3 -> {
                         if (isPaused) {
-                            synchronized (pauseLock) {
+                            synchronized (printLock) {
+                                synchronized (pauseLock) {
                                 isPaused = false;
                                 pauseLock.notify();
                                 System.out.println("Симуляция продолжена.");
+                                }
                             }
                         } else {
                             System.out.println("\nОшибка! Вы уже вышли из паузы!\n");
@@ -126,4 +101,37 @@ public class Main {
             }
         scanner.close();
         }
+
+    private static Thread createSimulationThread(Simulation simulation) {
+        return new Thread(() -> {
+            while (!isEnd) {                                         //цикл организован здесь а не в классе Simulation, флаг isEnd подтягивается оттуда
+                synchronized (pauseLock) {
+                    try {
+                        while (isPaused) {
+                            pauseLock.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+
+                if (!isEnd) {
+                    simulation.startSimulation();                  //одна отрисовка мапы (один ход)
+                }
+
+                try {
+                    Thread.sleep(1200); // Задержка для визуализации
+                } catch (InterruptedException e) {
+                    System.out.println("Поток simulationThread был прерван во время сна!");
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+                isEnd = simulation.isEnd();              //обновили флаг (условие выхода из цикла)
+            }
+            simulation.printLast();                      //вывели мапу в самом конце когда волк сожрал зайца
+            isExit = true;
+            System.out.println("Симуляция закончена! Введите цифру от 1 до 3 для выхода");
+        });
     }
+}
